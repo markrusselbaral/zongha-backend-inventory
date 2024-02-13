f934dee12266e790da9cd6a3c64fc3d53a10b110
anthony <anthony123oppus>
2024-02-13 23:48:56 +0800

    adddedRoute

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,10 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/login'
+        '/login',
+        '/logout',
+        '/api/category',
+        '/api/category/*',
+        '/api/items',
     ];
 }
