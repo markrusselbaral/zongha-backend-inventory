fef63ea20f12352647e57eb27f79b2b01d8cfec9
Nicole N. Amoguis <nicoleamoguis15@gmail.com>
2024-02-17 18:12:18 +0800

    product-updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,7 +19,7 @@ class Product extends Model
 
     public function item()
     {
-        return $this->belongsTo(Item::class, 'id');
+        return $this->belongsTo(Item::class, 'item_id');
     }
 
     public function allProducts()
@@ -31,7 +31,7 @@ class Product extends Model
     }
 
     public function warehouse(){
-        return $this->belongsTo(Warehouse::class, 'id');
+        return $this->belongsTo(Warehouse::class, 'warehouse_id');
     }
 
     public function allItems()
